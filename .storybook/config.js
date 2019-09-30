b1ec0a3e19a67dbda58e47b2b838d3028d43e55c
5tefc0de <st.muhle@gmail.com>
2019-09-30 14:23:49 +0200

    Finals Setup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(withInfo); 
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(require.context('../src', true, /\.stories\.js$/), module);
--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import styled from 'styled-components/macro'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppStyled>
       <h1>Hello World</h1>
-      </header>
-    </div>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  display: grid;
+  grid-template-rows: auto 48px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+`
+
 
 export default App;
